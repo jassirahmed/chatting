cfa8f83d92042dc0fa81d0b2ec32135881052347
saffan-ahmed-shaikh <saffan20102@gmail.com>
2022-03-08 18:16:00 +0530

    Safwan pull Request

--- Components/CreateSessionPage/CreateSession.js
+++ Components/CreateSessionPage/CreateSession.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../../styles/Home.module.scss";
-import Link from 'next/link'
+import Link from "next/link";
 import { useState } from "react";
 import { useEffect } from "react";
 import {
@@ -22,66 +22,66 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-} from '@chakra-ui/react'
-import { useDisclosure } from '@chakra-ui/react'
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { useRouter } from 'next/router'
-import { useCallback } from 'react';
-import { useNavigate } from "react-router-dom";
-
-
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 
 export default function CreateSession() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [addTextInput, setInputValue] = useState('');
-
+  const [addTextInput, setInputValue] = useState("");
+  const [data, setdata] = useState();
   const changeHandler = (e) => {
     setInputValue({
       [e.target]: e.target.value,
-    })
-
-  }
+    });
+  };
   const changeLength = () => {
     let textLength = document.getElementById("text").value;
     var count = textLength.length + 1;
 
     document.getElementById("count").innerHTML = count;
-    console.log(count)
-  }
+  };
 
-
-  console.log(textLength)
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault()
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
     var today = new Date();
     var date =
-      today.getFullYear() + '' + (today.getMonth() + 1) + '' + today.getDate();
+      today.getFullYear() + "" + (today.getMonth() + 1) + "" + today.getDate();
     var time =
       today.getHours() +
-      '' +
+      "" +
       today.getMinutes() +
-      '' +
+      "" +
       today.getSeconds() +
-      '' +
+      "" +
       today.getMilliseconds();
     var dateTime = date + time;
     let uniqueID = dateTime + Math.floor(Math.random() * 1000);
-    let data = {
-      type: 'createSession',
+    data = {
+      type: "createSession",
       message: addTextInput,
       uniqueID: uniqueID,
-    }
-    console.log(data)
-  }
-
+    };
+    const response = await fetch("http://localhost:3000/db/saveData", {
+      method: "POST",
+      body: JSON.stringify({ data }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const responseData = await response.json();
+    console.log("responseData", responseData);
+  };
 
   return (
-    <Box className={styles.createsessionpage} >
+    <Box className={styles.createsessionpage}>
       <Flex pt="120px" className={styles.hero_sec}>
-        <Box w="60%" bg={"#fff"} className={styles.textbox} p="15px" >
-          <FormControl as="form" p="4" textAlign="right" onSubmit={onSubmitHandler} >
+        <Box w="60%" bg={"#fff"} className={styles.textbox} p="15px">
+          <FormControl
+            as="form"
+            p="4"
+            textAlign="right"
+            onSubmit={onSubmitHandler}
+          >
             <Textarea
               id="text"
               required
@@ -93,9 +93,10 @@ export default function CreateSession() {
               placeholder="Paste Your Code Here"
               maxLength="5000"
             />
-            <Text fontSize="lg" color="#9e9e9e" >
+            <Text fontSize="lg" color="#9e9e9e">
               <span id="count">0</span>/5000
             </Text>
+            {/* <Link href="/ChattingSession"> */}
             <Button
               px="20px"
               py="5px"
@@ -105,11 +106,10 @@ export default function CreateSession() {
               type="submit"
               className={styles.submit_btn}
             >
-
               Create Session
             </Button>
-
-          </FormControl >
+            {/* </Link> */}
+          </FormControl>
         </Box>
         <Box w="40%" p={70} className={styles.btn_box} pt="0px">
           <Button
@@ -123,25 +123,33 @@ export default function CreateSession() {
           >
             Join Session
           </Button>
-          <Modal size='lg' isOpen={isOpen} onClose={onClose}>
+          <Modal size="lg" isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
-              <ModalHeader color='#fff' bg='#2b3954'>Join Session</ModalHeader>
-              <ModalCloseButton color='#fff' border='none' boxShadow='none' />
-              <ModalBody textAlign='center' fontSize='18px'>
-                <Input placeholder="Token ID" required type='number' maxLength='4' minLength='4' />
+              <ModalHeader color="#fff" bg="#2b3954">
+                Join Session
+              </ModalHeader>
+              <ModalCloseButton color="#fff" border="none" boxShadow="none" />
+              <ModalBody textAlign="center" fontSize="18px">
+                <Input
+                  placeholder="Token ID"
+                  required
+                  type="number"
+                  maxLength="4"
+                  minLength="4"
+                />
               </ModalBody>
 
-              <ModalFooter color='#fff'>
-                <Button bg='#6c757d' mr={3} onClick={onClose}>
+              <ModalFooter color="#fff">
+                <Button bg="#6c757d" mr={3} onClick={onClose}>
                   Cancel
                 </Button>
-                <Button bg='#28a745' >Join</Button>
+                <Button bg="#28a745">Join</Button>
               </ModalFooter>
             </ModalContent>
           </Modal>
         </Box>
-      </Flex >
-    </Box >
+      </Flex>
+    </Box>
   );
 }
